9ba0819af8d3d88b9e27d03a0011dd85f4fd92d1
Kotilainen Ilkka Antti <ilkka.a.kotilainen@gmail.com>
2023-04-07 16:05:12 +0800

    updated ArrayLength for ArrayList testing

--- src/smallProblems/ArrayLength.java
+++ src/smallProblems/ArrayLength.java
@@ -3,6 +3,9 @@
  */
 package smallProblems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author antiik.dev
  * @version 25 Dec 2022
@@ -15,9 +18,11 @@ public class ArrayLength {
      * @param args not in use
      */
     public static void main(String[] args) {
+        // Array
         int[] array = { 0, 1, 2, 3 };
         System.out.println("One dimensional array length: " + array.length);
         
+        // Two-dimensional array
         int[][] arrayDim = {
                 { 0, 1 },
                 { 2, 3 },
@@ -25,6 +30,14 @@ public class ArrayLength {
                 };
         System.out.println("Multidimensional length rows:   " + arrayDim.length);
         System.out.println("Multidimensional length column: " +arrayDim[0].length);
+        
+        // ArrayList
+        List<ArrayLength> lista = new ArrayList<ArrayLength>(3);
+        for (int i = 0; i < 3; i++) {
+            ArrayLength arr = new ArrayLength();
+            lista.add(arr);
+        }
+        System.out.println("Size: " + lista.size());
     
     }
 
---
+++ src/smallProblems/CharValues.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package smallProblems;
+
+/**
+ * @author ilkka
+ * @version 18 Mar 2023
+ *
+ */
+public class CharValues {
+
+    /**
+     * Main for testing
+     * @param args not in use
+     */
+    public static void main(String[] args) {
+        char k1 = 'F';
+        char k2 = 'A';
+        System.out.println((int)k1);
+        System.out.println((int)k2);
+    
+    }
+
+}
